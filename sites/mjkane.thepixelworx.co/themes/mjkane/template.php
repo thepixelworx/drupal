41f673b0b3c95598521efd52ced43e8ce77fe056
Tony Finlay <profile@thepixelworx.co>
2014-07-11 22:12:22 +0100

    MJKane Updates

--- sites/mjkane.thepixelworx.co/themes/mjkane/template.php
+++ sites/mjkane.thepixelworx.co/themes/mjkane/template.php
@@ -5,3 +5,17 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * MJ Kane theme.
  */
+
+
+function mjkane_menu_tree($variables) {
+  if (preg_match("/\main-submenu\b/i", $variables['tree'])){
+    return '<ul class="menu main-menu">' . $variables['tree'] . '</ul>';
+  } else {
+    return '<ul class="menu main-submenu">' . $variables['tree'] . '</ul>';
+  }
+}
+
+function mjkane_preprocess_html(&$vars) {
+    drupal_add_library('system', 'ui');
+ drupal_add_library('system', 'jquery');
+}
